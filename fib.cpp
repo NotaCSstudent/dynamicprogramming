8cdfbe5d3b70b209d1dfe291d150bbd83068970b
Mahmud Babu <mahmudbabu94@gmail.com>
2021-07-03 23:18:50 +0000

    added an array version instead

--- fib.cpp
+++ fib.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+//Using Memoziation 
+//Create Map
+// Key will be n
+// Value will be return 
 
-int fib(int n)
+int* memo = new int[1000];
+int fib(int n=0,unordered_map<int,int> memo = {})
 {
-  if(n<=2)  {return 1;}//Base Case
-
-  return fib(1);
+  if(n<=1)
+  {
+    return n;
+  }
+  else if(memo[n]!=0)
+  {
+    return memo[n];
+  }
+  else
+  {
+    memo[n] = fib(n-1) + fib(n-2);
+    return memo[n]; 
+  }
 }
 
 
@@ -13,5 +29,6 @@ int fib(int n)
 
 int main()
 {
-
+  cout << fib(6) << endl;
+  memo = nullptr;
 }
