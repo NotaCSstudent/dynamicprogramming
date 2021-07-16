3f8a97e0703dfcc7d680f3b1859033d32e3b3e47
NotaCSstudent <mahmudbabu94@gmail.com>
2021-07-16 16:24:29 -0400

    Update fib.cpp

--- C++/fib.cpp
+++ C++/fib.cpp
@@ -1,34 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
-//Using Memoziation 
-//Create Map
-// Key will be n
-// Value will be return 
 
-int* memo = new int[1000];
-int fib(int n=0,unordered_map<int,int> memo = {})
+
+
+
+
+
+
+
+//Recursion method
+int recur = 0;
+
+int fib_recursion(int n)
 {
-  if(n<=1)
-  {
-    return n;
-  }
-  else if(memo[n]!=0)
-  {
-    return memo[n];
-  }
-  else
-  {
-    memo[n] = fib(n-1) + fib(n-2);
-    return memo[n]; 
-  }
+    recur++;
+    if(n<=1)
+    {
+        return 1;
+    }
+    return fib_recursion(n-1) + fib_recursion(n-2);
 }
 
 
 
+//Memoization method
+int* temparr = new int[INT_MAX]; ///Temporary lookup table to cache the results
+int memoize = 0;
+
+
+int fib_memoization(int n, int* temp)
+{
+    memoize++;
+    if(n<=1)
+    {
+        temp[n] = 1;
+        return 1;
+    }
+    if(temp[n]!=0)
+    {
+        return temp[n];
+    }
+    temp[n] = fib_memoization(n-1,temp) + fib_memoization(n-2,temp);
+    return temp[n];
+    
+}
 
 int main()
 {
-  cout << fib(6) << endl;
-  memo = nullptr;
+    cout << "Fibbonacci using recursion " << fib_recursion(20) << endl;
+    cout <<  "Number of calls " << recur << endl;
+    cout << "Fibonacci using memoization " << fib_memoization(20,temparr) << endl;
+    cout << "Number of calls " << memoize << endl;
 }
